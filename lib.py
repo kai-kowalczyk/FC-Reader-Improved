4ac0f03046a6b50246b22c9300a51cb042cb9899
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-11-23 21:53:26 +0100

    final version

--- lib.py
+++ lib.py
@@ -7,112 +7,151 @@ class Reader:
 
     ALLOWED_EXTENSIONS = ('csv', 'json', 'pickle')
 
-    def __init__(self, path, destination, changes, filetype=''):
+    def __init__(self, path, destination, changes):
         self.filename = Path(path).name
         self.path = path
         self.parent_path = Path(self.path).parent  
         self.destination = destination
         self.changes = changes
         self.cwd = Path.cwd()
-        self.filetype = filetype
-        self.validated = self.validate_source()
+        self.src_filetype = self.set_filetype(self.filename)
+        self.dest_filetype = self.set_filetype(Path(self.destination).name)
+        self.src_validated = self.validate_source()
+        self.dest_validated = self.validate_destination()
+        self.file_data = self.read_data()
+        self.mod_file()
+        self.create_output()
 
     def validate_source(self):
         if Path(self.path).exists():
             if Path(self.path).is_file():
-                if self.filetype not in self.ALLOWED_EXTENSIONS:
+                if self.src_filetype not in self.ALLOWED_EXTENSIONS:
                     print('Nieobsługiwany format.')
-                    return 'wrong_ext'
+                    error = 'wrong_ext'
+                    self.show_files_in_dir(error)
                 else:
-                    return f'valid_{self.filetype}'
+                    return f'valid_{self.src_filetype}'
             else:
-                return 'valid_path'
+                error = 'valid_path'
+                self.show_files_in_dir(error)
         else:
             if Path(self.parent_path).is_dir():
-                return 'valid_parent_path'
+                error = 'valid_parent_path'
+                self.show_files_in_dir(error)
             else:
-                return 'invalid_path'
+                error = 'invalid_path'
+                self.show_files_in_dir(error)
 
     def validate_destination(self):
-        ext = Path(self.destination).suffix[1:]
         dest_parent = Path(self.destination).parent
-        if ext not in self.ALLOWED_EXTENSIONS:
+        if self.dest_filetype not in self.ALLOWED_EXTENSIONS:
             print('Podano złe rozszerzenie pliku wyjściowego. Obsługiwane formaty: .csv, .json, .pickle')
             quit()
         elif not Path(dest_parent).exists():
             print(f'Podana ścieżka pliku wyjściowego {self.destination} nie istnieje.')
             quit()
+        else:
+            return True
 
-    '''def set_filetype(self):
-        return Path(self.filename).suffix[1:]'''
+    def set_filetype(self, file):
+        return Path(file).suffix[1:]
 
     def validate_change(self, element):
         splitted_element = element.split(',')
         row_index = int(splitted_element[0])
         column_index = int(splitted_element[1])
         change_value = splitted_element[2]
-        return row_index, column_index, change_value   
+        return row_index, column_index, change_value
 
-    def show_files_in_dir(self):
-        if self.validated == 'wrong_ext':
+    def mod_file(self):
+        try:
+            for element in self.changes:
+                row_index, column_index, change_value = self.validate_change(element)
+                self.file_data[row_index][column_index] = str(change_value)
+        except IndexError:
+            print('Wprowadzono nr rzędu/nr kolumny, które nie istnieją w pliku źródłowym. Wprowadź poprawne dane.')
+            quit()
+
+    def read_data(self):
+        if self.src_filetype == 'csv':
+            csv_src = CSVReader()
+            return csv_src.get_csv_data(self.path)
+        elif self.src_filetype == 'json':
+            json_src = JSONReader()
+            return json_src.get_json_data(self.path)
+        elif self.src_filetype == 'pickle':
+            pickle_src = PickleReader()
+            return pickle_src.get_pickle_data(self.path) 
+
+    def create_output(self):
+        if self.dest_filetype == 'csv':
+            csv_dest = CSVReader()
+            csv_dest.csv_output(self.file_data, self.destination)
+        elif self.dest_filetype == 'json':
+            json_dest = JSONReader()
+            json_dest.json_output(self.file_data, self.destination)
+        elif self.dest_filetype == 'pickle':
+            pickle_dest = PickleReader()
+            pickle_dest.pickle_output(self.file_data, self.destination)
+
+    def show_files_in_dir(self, error):
+        if error == 'wrong_ext':
             print('Podano złe rozszerzenie pliku wejściowego. Dostępne pliki .csv, .json i .pickle w katalogu nadrzędnym to: ')
             print(sorted(Path(self.parent_path).glob('*.csv', '*.json', '*.pickle')))
-        elif self.validated == 'valid_path':
+        elif error == 'valid_path':
             print('Nie podano nazwy pliku wejściowego. Dostępne pliki .csv, .json i .pickle w podanym katalogu to: ')
             print(sorted(Path(self.path).glob('*.csv', '*.json', '*.pickle')))
-        elif self.validated == 'valid_parent_path':
+        elif error == 'valid_parent_path':
             print('Podano złą ścieżkę pliku wejściowego. Dostępne pliki .csv, .json i .pickle w katalogu nadrzędnym to: ')
             print(sorted(Path(self.parent_path).glob('*.csv')))
         else:
             print('Podano złą ścieżkę pliku wejściowego. Dostępne pliki .csv, .json i .pickle w bieżącym katalogu to: ')
             print(sorted(Path(self.cwd).glob('*.csv', '*.json', '*.pickle')))
 
+
 class CSVReader(Reader):
 
-    def get_csv_data(self):
-        with open(self.path, 'r', newline='') as file:
+    def __init__(self):
+        pass
+
+    def get_csv_data(self, src_file):
+        print('get csv data')
+        with open(src_file, 'r', newline='') as file:
             reader = csv.reader(file, delimiter=';')
             file_data = []
             for row in reader:
                 file_data.append(row)
         return file_data
 
-    def mod_file(self, file_data, row_index, column_index, change_value):
-        file_data[row_index][column_index] = str(change_value)
-
-    def create_output(self, file_data):
-        self.validate_destination()
-        with open(self.destination, 'w') as file:
+    def csv_output(self, data, dest_file):
+        with open(dest_file, 'w') as file:
             writer = csv.writer(file, delimiter=';')
-            writer.writerows(file_data)
+            writer.writerows(data)
 
 class JSONReader(Reader):
+
+    def __init__(self):
+        pass
     
-    def get_json_data(self):
-        with open(self.path, 'r', newline='') as file:
-            reader = json.load(file)
-            keys = []
-            values = []
-            for item in reader.keys():
-                keys.append(item)
-            for item in reader.values():
-                values.append(item)
-        return[keys, values]
+    def get_json_data(self, src_file):
+        with open(src_file, 'r', newline='') as file:
+            file_data = json.load(file)
+        return file_data
 
-    def mod_file(self, file_data, row_index, column_index, change_value):
-        file_data[column_index][row_index] = str(change_value)
-
-    def create_output(self, file_data):
-       self.validate_destination()
-       out_data = dict(zip(file_data[0], file_data[1]))
-       print(out_data)
-       with open(self.destination, 'w') as file:
-           json.dump(out_data, file)
-
-
-
+    def json_output(self, data, dest_file):
+        with open (dest_file, 'w') as file:
+            json.dump(data, file)
 
 class PickleReader(Reader):
-    
-    def get_pickle_data(self):
+
+    def __init__(self):
         pass
+    
+    def get_pickle_data(self, src_file):
+        with open(src_file, 'rb') as file:
+            file_data = pickle.load(file)
+        return file_data
+
+    def pickle_output(self, data, dest_file):
+        with open(dest_file, 'wb') as file:
+            pickle.dump(data, file)
--- reader.py
+++ reader.py
@@ -1,42 +1,14 @@
 import sys
-from pathlib import Path
-from lib import Reader, CSVReader, JSONReader, PickleReader
+from lib import Reader
 
-source = sys.argv[1]
-destination = sys.argv[2]
-changes = sys.argv[3:]
-
-'''changes "Y,X,wartosc" Y - wiersz(od 0), X - kolumna (od 0), wartosc - modyfikacja do wpisania w komórkę'''
-
-#reader = Reader(source, destination, changes)
-
-def set_filetype():
-    return Path(source).suffix[1:]
-
-if set_filetype() == 'csv':
-    reader = CSVReader(source, destination, changes, 'csv')
-    if reader.validated == 'valid_csv':
-        file_data = reader.get_csv_data()
-        for element in reader.changes:
-            row_index, column_index, change_value = reader.validate_change(element)
-            reader.mod_file(file_data, row_index, column_index, change_value)
-        reader.create_output(file_data)
-    else:
-        reader.show_files_in_dir()
-
-elif set_filetype() == 'json':
-    reader = JSONReader(source, destination, changes, 'json')
-    if reader.validated == 'valid_json':
-        file_data = reader.get_json_data()
-        for element in reader.changes:
-            row_index, column_index, change_value = reader.validate_change(element)
-            reader.mod_file(file_data, row_index, column_index, change_value)
-        reader.create_output(file_data)
-        print(file_data)
-
-elif set_filetype() == 'pickle':
-    pass
-
-else:
+try:
+    source = sys.argv[1]
+    destination = sys.argv[2]
+    changes = sys.argv[3:]
     reader = Reader(source, destination, changes)
-    reader.show_files_in_dir()
+except IndexError:
+    print('Uruchom program wpisując "python3 reader.py [source] [destination][nr rzędu,nr kolumny,zmiana do wprowadzenia]". Zmian możesz wprowadzić więcej niż jedną. Numery rzędów i kolumn policz zaczynając od 0.')
+
+
+
+
